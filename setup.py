acac163c6f2ce8eab1e90f36b999e8f1d9be908f
Luis María Pizarro <lmpizarro@gmail.com>
2016-02-16 09:26:14 -0300

    setup.py cont.

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@
 
 from setuptools import setup
 
-setup(name='funniest',
+
+
+setup(name='qeXml',
     version='0.1',
     description='A quantum espresso xml generator',
     url='https://github.com/lmpizarro/espresso-xml',
     author='Luis María Pizarro',
     author_email='lpizarro@cnea.gov.ar',
     license='GNU',
-    packages=['testing'],
+    packages=['qeXml'],
     zip_safe=False)
 
 
+''' References:
+
+    http://python-packaging.readthedocs.org/en/latest/minimal.html
+'''
