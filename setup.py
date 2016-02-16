d82bf01ebafbb5d755f1c257066c1ad80f69eedc
Luis María Pizarro <lmpizarro@gmail.com>
2016-02-16 09:18:37 -0300

    setup.py init

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='funniest',
+    version='0.1',
+    description='A quantum espresso xml generator',
+    url='https://github.com/lmpizarro/espresso-xml',
+    author='Luis María Pizarro',
+    author_email='lpizarro@cnea.gov.ar',
+    license='GNU',
+    packages=['testing'],
+    zip_safe=False)
+
+
